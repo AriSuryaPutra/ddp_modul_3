3f5ae2718cc925ac308eed9ab026d1167c4ab26a
Ari <putra.kadek.ari@gmail.com>
2020-11-25 11:56:28 +0800

    revisi

---
+++ tugas_pendahuluan/2_contoh_program_aritmatika.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,c,d,e,f,g;
+
+    a = 5;
+    b = 2;
+    c = a+b;
+    d = a-b;
+    e = a*b;
+    f = a/b;
+    g = a%b;
+
+    cout << "Nilai a + b = " << c <<endl;
+    cout << "Nilai a - b = " << d <<endl;
+    cout << "Nilai a * b = " << e <<endl;
+    cout << "Nilai a / b = " << f <<endl;
+    cout << "Nilai a %% b =" << g <<endl;
+    
+    return 0;
+}
